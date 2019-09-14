e300278bc28b755c2ddfc6a55a6bf9a32c64dd5b
reubengt <reubengeorgethomas@gmail.com>
2019-09-14 18:45:14 +0100

    add readabilityParser function

---
+++ src/helpers/readabilityParser.js
@@ -0,0 +1,12 @@
+const fetch = require("node-fetch");
+const Readability = require("readability");
+const { JSDOM } = require("jsdom");
+
+module.exports = async url => {
+  const fetchResponse = await fetch(url);
+  const pageHTML = await fetchResponse.text();
+  const doc = new JSDOM(pageHTML, { url });
+  const reader = new Readability(doc.window.document);
+  const article = reader.parse();
+  return article;
+};
