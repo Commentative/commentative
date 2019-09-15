5ba7980adfe67a89dfb4dc67df3eb6aa5edb3bcd
Jamie Brown <jamiebrown201@hotmail.com>
2019-09-15 13:40:59 +0100

    add comments

--- src/views/js/newarticle.js
+++ src/views/js/newarticle.js
@@ -1,4 +1,8 @@
 let articleParagraphs = document.querySelectorAll(".article > div > div > *");
+let submitCommentForm = document.querySelector(".submitComments");
+let firstSubmit = true;
+let url =
+  "https://8rj0xswzt3.execute-api.eu-west-1.amazonaws.com/dev/commentative";
 
 window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
@@ -18,3 +22,61 @@ window.addEventListener("scroll", event => {
     }
   });
 });
+
+function addIndexToParagraphs() {
+  articleParagraphs.forEach((element, index) => {
+    element.setAttribute("data-article-element-index", index + 1);
+  });
+}
+
+function addComment() {
+  document.querySelector(".addComment").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const reference = document
+      .querySelector(".selected")
+      .getAttribute("data-article-element-index");
+    const body = document.querySelector(".addCommentText").value;
+    const user = "Jamie";
+    if (firstSubmit) {
+      const articleBody = document.querySelector(".articleBody").value;
+      firstSubmit = false;
+      const params = { user, articleBody, commentData: [{ body, reference }] };
+      fetch(url, {
+        method: "POST",
+        body: JSON.stringify(params),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          return res.text();
+        })
+        .then(res => {
+          const result = JSON.parse(res);
+          history.pushState(null, "", "/" + result.uuid);
+        });
+    } else {
+      const path = window.location.pathname.split("/");
+      const uuid = path[path.length - 1];
+      const params = { user, commentData: [{ body, reference }] };
+      console.log("hello", url + "/" + uuid);
+      fetch(url + "/" + uuid, {
+        method: "PUT",
+        body: JSON.stringify(params),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(res => {
+          console.log(res);
+          return res.text();
+        })
+        .then(res => {
+          console.log(res);
+        });
+    }
+  });
+}
+
+addIndexToParagraphs();
+addComment();
