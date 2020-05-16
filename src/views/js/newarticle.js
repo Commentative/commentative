84c4fe8fc9d05330efa72757fee1c7acbc85c83c
Maurice <mbanerjeepalmer@gmail.com>
2020-05-16 18:48:55 +0100

    Add comments to explain the code.

--- src/views/js/newarticle.js
+++ src/views/js/newarticle.js
@@ -4,7 +4,11 @@ let firstSubmit = true;
 let url =
   "https://8rj0xswzt3.execute-api.eu-west-1.amazonaws.com/dev/commentative";
 
-window.addEventListener("scroll", event => {
+// This is an ongoing check to see which element is in the middle of the screen.
+// Then it adds a class 'selected'.
+// .selected is the state used so styling knows what to highlight
+// and so commenting functionality knows which paragraph index to set as the reference 
+  window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
   articleParagraphs.forEach(para => {
     let paragraphHeight = para.offsetHeight;
@@ -23,11 +27,14 @@ window.addEventListener("scroll", event => {
   });
 });
 
+// This is a one-off process that happens when an article is first added.
+// TODO: Perhaps this should happen as part of the Readability parser process, rather than here.
 function addIndexToParagraphs() {
   articleParagraphs.forEach((element, index) => {
     element.setAttribute("data-article-element-index", index + 1);
   });
 }
+
 
 function addComment() {
   document.querySelector(".addComment").addEventListener("click", function(e) {
@@ -35,10 +42,16 @@ function addComment() {
     const reference = document
       .querySelector(".selected")
       .getAttribute("data-article-element-index");
+    
     const body = document.querySelector(".addCommentText").value;
-    const user = "Jamie";
+    const user = "🖊️";
     if (firstSubmit) {
+      
+      // TODO: Don't understand this. Please explain in documentation.
       const articleBody = document.querySelector(".articleBody").value;
+      
+      // TODO: This `firstSubmit` thing feels weird. 
+      // Why does the user go through different journeys depending on whether they've submitted already?
       firstSubmit = false;
       const params = { user, articleBody, commentData: [{ body, reference }] };
       fetch(url, {
