7b8843ec2e95c09d0cb4545ae020f076abb298ae
reubengt <reubengeorgethomas@gmail.com>
2020-05-26 16:04:21 +0100

    change first submission to dynamodb to be right after readability parse, rather than from frontend - reloading uuid link works

--- src/app.js
+++ src/app.js
@@ -26,27 +26,56 @@ app.set("port", process.env.PORT || 3000);
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
   res.render("home", { page: "home" });
 });
 
-app.get("/newarticle", async (req, res) => {
-  const url = req.query.contenturl;
-  const articleObj = await readabilityParser(url);
+app.post("/newarticle", async (req, res, next) => {
+  const articleUrl = req.body.contenturl;
+  const articleBody = await readabilityParser(articleUrl);
+  console.log("article body after readability", articleBody);
+  const backendUrl =
+    "https://8rj0xswzt3.execute-api.eu-west-1.amazonaws.com/dev/commentative";
+  //no user and no commentData because at this point we're just submitting the article Body
+  const params = { user: null, commentData: [], articleBody };
+  axios
+    .post(backendUrl, params)
+    .then((res) => res.data)
+    .then((articleObj) => {
+      //   console.log(articleObj);
+      res.redirect(`/${articleObj.uuid}`);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+app.get("/:uuid", async (req, res) => {
+  const url = `https://8rj0xswzt3.execute-api.eu-west-1.amazonaws.com/dev/commentative/${req.params.uuid}`;
+  const result = await axios(url);
+  const articleObj = {
+    header: "This is the actual header of the article",
+    content: result.data.articleBody.content,
+    siteName: result.data.articleBody.siteName,
+    title: result.data.articleBody.title,
+    comments: result.data.comments,
+  };
+  //   console.log(articleObj);
   res.render("newarticle", { articleObj, page: "newarticle" });
 });
 
-// app.get("/:uuid", async (req, res) => {
-//   const url = `https://8rj0xswzt3.execute-api.eu-west-1.amazonaws.com/dev/commentative/${req.params.uuid}`;
-//   const result = await axios(url);
-//   const articleObj = {
-//     header: "This is the actual header of the article",
-//     content: result.data.articleBody,
-//     comments: result.data.comments
-//   };
-//   console.log(articleObj);
-//   res.render("newarticle", { articleObj, page: "newarticle" });
-// });
+app.get("*", (req, res) => {
+  res.status(404).send("page not found");
+});
+
+app.use((error, req, res, next) => {
+  //   console.log(error);
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong";
+  const name = error.name || "Error";
+  const errorObj = { name: name, message: message };
+  res.status(status).send(errorObj);
+});
 
 app.listen(app.get("port"), () => {
   console.log("App running on port", app.get("port"));
